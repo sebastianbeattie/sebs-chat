1928f19978520a6a1d9054e0d63f7182a112d0b5
ElementalMP4 <sebahudl@gmail.com>
2025-05-24 13:38:04 +0100

    Add file output option

--- client/main.go
+++ client/main.go
@@ -14,6 +14,7 @@ var (
 	config     Config
 	configPath string
 	inputPath  string
+	outputPath string
 )
 
 func main() {
@@ -67,11 +68,11 @@ func main() {
 			if err != nil {
 				return fmt.Errorf("error marshalling encrypted message: %v", err)
 			}
-			fmt.Println(base64.StdEncoding.EncodeToString(output))
-			return nil
+			return writeOutput(base64.StdEncoding.EncodeToString(output))
 		},
 	}
 	encryptCmd.Flags().StringVarP(&inputPath, "input", "i", "", "Path to input JSON file")
+	encryptCmd.Flags().StringVarP(&outputPath, "output", "o", "", "Path to output file (optional)")
 	encryptCmd.MarkFlagRequired("input")
 	rootCmd.AddCommand(encryptCmd)
 
@@ -88,15 +89,16 @@ func main() {
 			if err != nil {
 				return fmt.Errorf("error decrypting message: %v", err)
 			}
-			fmt.Printf("%s: %s\n", decryptedMessage.Author, decryptedMessage.RawText)
-			return nil
+			output := fmt.Sprintf("%s: %s\n", decryptedMessage.Author, decryptedMessage.RawText)
+			return writeOutput(output)
 		},
 	}
 	decryptCmd.Flags().StringVarP(&inputPath, "input", "i", "", "Path to input file")
+	decryptCmd.Flags().StringVarP(&outputPath, "output", "o", "", "Path to output file (optional)")
 	decryptCmd.MarkFlagRequired("input")
 	rootCmd.AddCommand(decryptCmd)
 
-	rootCmd.AddCommand(&cobra.Command{
+	exportCmd := &cobra.Command{
 		Use:   "export-key",
 		Short: "Export your public key",
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -108,10 +110,11 @@ func main() {
 			if err != nil {
 				return fmt.Errorf("error marshalling key exchange: %v", err)
 			}
-			fmt.Println(base64.StdEncoding.EncodeToString(output))
-			return nil
+			return writeOutput(base64.StdEncoding.EncodeToString(output))
 		},
-	})
+	}
+	exportCmd.Flags().StringVarP(&outputPath, "output", "o", "", "Path to output file (optional)")
+	rootCmd.AddCommand(exportCmd)
 
 	importCmd := &cobra.Command{
 		Use:   "import-key",
@@ -130,6 +133,7 @@ func main() {
 		},
 	}
 	importCmd.Flags().StringVarP(&inputPath, "input", "i", "", "Path to input file")
+	importCmd.Flags().StringVarP(&outputPath, "output", "o", "", "Path to output file (optional)")
 	importCmd.MarkFlagRequired("input")
 	rootCmd.AddCommand(importCmd)
 
@@ -137,4 +141,12 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func writeOutput(output string) error {
+	if outputPath != "" {
+		return os.WriteFile(outputPath, []byte(output), 0644)
+	}
+	fmt.Println(output)
+	return nil
 }
